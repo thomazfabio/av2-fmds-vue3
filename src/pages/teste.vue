083564defe60091f7f24b7e15f8f04c04ffa029d
fabio thomaz <fabio_thomaz@live.com>
2024-05-17 22:23:32 -0300

    ponto de salvamento padrão

--- src/pages/teste.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="d-flex justify-center align-center" style="width: 100%; height: 100%; background-color: aliceblue;">
-        <div style="width: 600px; height: 400px; background-color: black;"></div>
-    </div>
-</template>
-
-<script setup>
-</script>
